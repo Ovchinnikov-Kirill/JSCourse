9057f39428405e3f033ab996471cff32038779a0
OvchinnikovKirill <Ovchinnikovk758@gmail.com>
2020-12-25 23:48:57 +0200

    if, while, for - homework is complette

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const porsonalMovieDB = {
     count : numberOfFilms,
@@ -10,12 +10,61 @@ const porsonalMovieDB = {
     privat : false
 };
 
-const a = prompt('Один из просмотреных фильмов?', ' '),
-    b = +prompt('На скольо оцените его?', ' '),
-    c = prompt('Один из просмотреных фильмов?', ' '),
-    d = +prompt('На скольо оцените его?', ' ');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотреных фильмов?', ''),
+        b = +prompt('На скольо оцените его?', '');
 
-    porsonalMovieDB.movies[a] = b;
-    porsonalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        porsonalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }    
+}
+
+// Разные варианты циклов
+
+
+//  let i = 0;
+
+// while (i < 2) {
+//     const a = prompt('Один из просмотреных фильмов?', ''),
+//         b = +prompt('На скольо оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//         porsonalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+//     i++;  
+// }
+
+// do {
+//     const a = prompt('Один из просмотреных фильмов?', ''),
+//           b = +prompt('На скольо оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//         porsonalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+//     i++;  
+// } while (i < 2);
+
+
+if (porsonalMovieDB.count < 10) {
+    alert('Просмотренно дольно мало фильмов');
+} else if (porsonalMovieDB.count >= 10 && porsonalMovieDB.count <= 30) {
+    alert('Вы класический зритель');
+} else if (porsonalMovieDB.count > 30) {
+    alert('Да вы настоящий киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(porsonalMovieDB);
