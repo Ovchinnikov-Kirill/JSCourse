2e4b8cc53b2ac2172ea729691be2cfad3f7ca310
OvchinnikovKirill <Ovchinnikovk758@gmail.com>
2020-12-25 20:20:55 +0200

    moviesDB first step

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
+
+const porsonalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const a = prompt('Один из просмотреных фильмов?', ' '),
+    b = +prompt('На скольо оцените его?', ' '),
+    c = prompt('Один из просмотреных фильмов?', ' '),
+    d = +prompt('На скольо оцените его?', ' ');
+
+    porsonalMovieDB.movies[a] = b;
+    porsonalMovieDB.movies[c] = d;
+
+console.log(porsonalMovieDB);
